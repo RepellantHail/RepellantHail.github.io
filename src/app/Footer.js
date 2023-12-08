00699e0986b900211d555c1795e9ddaa0ff5d0c3
RepellantHail <a20310068@ceti.mx>
2023-12-07 19:34:37 -0600

    svg added

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -1,14 +1,14 @@
 export default function Footer() {
     return (
         <div className="container-fluid text-center">
-            <div className="row">
+            <div className="row m-3">
                 <div className="col-8">
                     <h4>Luis Modesto</h4>
                     <p><i class="fa-solid fa-laptop-code"></i> Software Engineer Student</p>
                 </div>
-                <div className="col-4">
+                <div className="col-4" >
                     <h4>Social</h4>
-                    <div className="d-flex justify-content-evenly">
+                    <div className="d-flex justify-content-between">
                         <a className="icon-link" href="https://www.linkedin.com/in/jearim-modesto/">
                             <i class="bi bi-linkedin"> </i>        
                             LinkedIn                      
@@ -16,11 +16,15 @@ export default function Footer() {
                         <a className="icon-link" href="https://github.com/RepellantHail">
                             <i class="bi bi-github"> </i>
                             Github
-                        </a>                        
+                        </a>     
+                        <a className="icon-link" href="https://github.com/RepellantHail">                            
+                            <img src='./leetcode.svg' alt="My SVG" style={{ margin: 0, width: 20, height: 20, color: 'red' }}/>
+                            LeetCode
+                        </a>                     
                     </div>
                 </div>
             </div>
-            <div className="row">
+            <div className="row m-5">
                 <div className="col d-flex justify-content-evenly">
                     <h6>Built with: <i class="fa-brands fa-react"></i> React.js  Next.js </h6>
                     <h6>Copyright © Luis Modesto 2023 </h6>
