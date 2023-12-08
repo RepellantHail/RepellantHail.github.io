9ea467d192838e270d2059856a91db007e8c5124
RepellantHail <a20310068@ceti.mx>
2023-12-08 12:29:45 -0600

    added custom font

--- src/app/Main.js
+++ src/app/Main.js
@@ -3,7 +3,10 @@ export default function Main(){
         <div className="container-fluid w-100 text-center p-3 m-5">
             <div className='row'>
                 <div className="col-6 w-50 p-5">
-                    <h1 className="m-3 fs-1">Luis Modesto</h1>
+                    <h1 
+                        className="m-3 fs-1"
+                        style={{ fontFamily: 'Pixelify_Sans', fontWeight: 'bold' }}
+                    >Luis Modesto</h1>
                     <h3 className="m-3">Software Engineer</h3>
                     <p className='w-100 m-3 text-center'>
                     I'm Luis Modesto, a Software Developer passionate about learning and exploring technologies that bring creative ways to resolve problems.
