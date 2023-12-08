1919fa665f9b2f0298e6bbb9a6e893120e0cfda6
RepellantHail <a20310068@ceti.mx>
2023-12-08 16:39:43 -0600

    added shadows for title

--- src/app/components/Title.js
+++ src/app/components/Title.js
@@ -3,7 +3,13 @@ import React from 'react'
 export default function Title({title}) {
   return (
     <h1
-        style={{ fontFamily: 'Pixelify_Sans', fontWeight: 'bold' }}> 
+    className='m-3'
+        style={{ 
+            fontFamily: 'Pixelify_Sans', 
+            fontWeight: 'bold', 
+            fontSize: '5em',
+            textShadow: '0 0 3px #0C356A'
+            }}> 
         {title}
     </h1>
   )
