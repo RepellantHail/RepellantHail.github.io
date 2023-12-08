859d25b08f5517cffc8c3969df0b9a494e5e53ea
RepellantHail <a20310068@ceti.mx>
2023-12-07 22:26:50 -0600

    home page set up

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,11 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div className="container-fluid text-center vh-100 d-flex flex-column ">
-        <div className="row align-items-start flex-grow-1 h-75">
-        </div>
+    <div className="container-fluid text-start vh-50 d-flex flex-column ">
+      <div className='m-2'>
+        <h1>Software Engineer</h1>
+        <p className='w-50 m-5'>I'm Luis Modesto, a Software Developer passionate about leraning and exploring technologies that brings creative ways to resolve probles</p>
+      </div>        
     </div>
   );
 }
