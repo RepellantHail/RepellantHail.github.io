58cee3cc9b1a6497956871bfefb243fad3bcdf4c
RepellantHail <a20310068@ceti.mx>
2023-12-11 18:41:21 -0600

    styling patch

--- src/app/Main.js
+++ src/app/Main.js
@@ -4,14 +4,14 @@ export default function Main(){
     return(
         <div className="container-fluid w-100 text-center p-3 m-5">
             <div className='row'>
-                <div className="col-6 w-50 p-5">
+                <div className="col p-5">
                     <Title {...{ title: "Luis Modesto" }} />
                     <h3 className="m-3"><i class="fa-solid fa-laptop-code"></i> Software Engineer</h3>
                     <p className='w-100 m-3 text-center'>
                     I'm Luis Modesto, a Software Developer passionate about learning and exploring technologies that bring creative ways to resolve problems.
                     </p>
                 </div>
-                <div className="col-6 text-center p-3">
+                <div className="col text-center p-3">
                     <img
                         src='./images/developer-profile-picture.png'
                         alt="Profile Picture"
--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar(){
     return(
-        <nav className="navbar navbar-expand-lg bg-dark border-bottom" data-bs-theme="dark">
+        <nav className="navbar navbar-expand-lg bg-dark border-bottom w-100 p-1" data-bs-theme="dark">
             <Link href={"/"}><i class="fa-solid fa-house"></i> Home</Link>
             <Link href={"/about"}><i class="fa-solid fa-circle-user"></i> About</Link>
             <Link href={"/portfolio"}><i class="fa-solid fa-briefcase"></i> Portfolio</Link>
